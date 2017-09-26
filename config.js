d7779166e90ebafdff63ed24efa04e7719aaecb9
robinstark <robin@volunteerhq.org>
2017-09-26 14:10:55 +1300

    Config and sample bucket policy

--- config.js
+++ config.js
@@ -1,24 +1,14 @@
 "use strict";
 
 var config = {
-    "attachmentsBucket": "mybucket.mydomain.com",
-    "attachmentsPrefix": "attachments",
+    "attachmentsBucket": process.env.ATTACHMENTS_BUCKET,
+    "attachmentsPrefix": process.env.ATTACHMENTS_PREFIX,
     "debug": true,
     "rules": { // first match
-        "^mail.example.com$": { // exact "Object key prefix" match
-            "to": "somerecipient@yourdomain.com",
-            "from": "mail <mail@yourdomain.com>",
-            "subject": "[mail] ",
-        },
-        "example.com": { // matches any "Object key prefix" containing "example.com"
-            "to": "somerecipient+wildcard@yourdomain.com",
-            "from": "mail <mail@yourdomain.com>",
-            "subject": "",
-        },
-        ".*": { // matches everything - don't forget the dot!
-            "to": "catchall@yourdomain.com",
-            "from": "mail <mail@yourdomain.com>",
-            "subject": "[catchall] ",
+        "^info-ritascholarship.org$": { // exact "Object key prefix" match
+            "to": process.env.DESTINATION_EMAIL,
+            "from": process.env.SOURCE_EMAIL,
+            "subject": process.env.SUBJECT_PREFIX,
         }
     }
 }
